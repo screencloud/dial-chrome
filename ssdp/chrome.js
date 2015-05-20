b576d9b0d7ccba2e88f904e01cfcf5df4ecd1454
Jirasak Saebang <cosmodesktop@gmail.com>
2015-05-20 12:06:24 +0700

    remove packages

--- ssdp/chrome.js
+++
@@ -1,9 +0,0 @@
-// module interface for chrome os
-
-var udp = require('./lib/udp/udp-chrome.js');
-
-module.exports = {
-    createDevice: require('./lib/device.js')(udp)
-};
-
-// createClient: require('./lib/client.js')(udp),
--- ssdp/lib/ssdp-constants.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    SSDP_PORT: 1900,
-    SSDP_MULTICAST_IP: '239.255.255.250'
-};
